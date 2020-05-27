1d7ff3ee37e10eea6c79547a8942d4872fa41d79
osiris-sys <65527890+osiris-sys@users.noreply.github.com>
2020-05-26 23:30:43 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/stock-exchange.jpg') {
+      myImage.setAttribute ('src','images/firefox2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/stock-exchange.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
